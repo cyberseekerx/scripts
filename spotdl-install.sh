340d8be777b60a14461a7e2f89a8aa2135edfb65
cyberseekerx <kanekarayush777@proton.me>
2025-07-01 13:59:12 +0530

    Update spotdl-install.sh

--- spotdl-install.sh
+++ spotdl-install.sh
@@ -1,17 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Create music folder
-mkdir -p /storage/emulated/0/Music/SpotDL
-mkdir -p /storage/emulated/0/.Ayush_spot_script_cache 
-source ~/.bashrc
-
-# this is making files 
-echo 'alias gotospot="cd /storage/emulated/0/Music/SpotDL"' >> ~/.bashrc
-source ~/.bashrc
-cd /storage/emulated/0/.Ayush_spot_script_cache 
-curl -sL https://raw.githubusercontent.com/cyberseekerx/scripts/refs/heads/main/spotdl-install.sh -o spot.sh 
-echo 'alias spot="cd /storage/emulated/0/.Ayush_spot_script_cache && bash spot.sh"' >> ~/.bashrc
-
 # Update and install required tools
 pkg update -y && pkg install wget ffmpeg -y
 #note that thare are importent file ".Ayush_spot_script_cache" don't delete them if using spotdl 
