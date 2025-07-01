913c49c1c60405435044221954879cb8f41fa4c1
cyberseekerx <kanekarayush777@proton.me>
2025-07-01 13:39:29 +0530

    Update spotdl-install.sh

--- spotdl-install.sh
+++ spotdl-install.sh
@@ -11,7 +11,7 @@ source ~/.bashrc
 echo 'alias gotospot="cd /storage/emulated/0/Music/SpotDL"' >> ~/.bashrc
 source ~/.bashrc
 cd /storage/emulated/0/.Ayush_spot_script_cache 
-curl -sL https://raw.githubusercontent.com/cyberseekerx/scripts/refs/heads/main/spotdl-install.sh -o
+curl -sL https://raw.githubusercontent.com/cyberseekerx/scripts/refs/heads/main/spotdl-install.sh -o spot.sh 
 echo 'alias spot="cd /storage/emulated/0/.Ayush_spot_script_cache && bash spot.sh"' >> ~/.bashrc
 #note that thare are importent file ".Ayush_spot_script_cache" don't delete them if using spot 
 # Install SpotDL using official script
