f6a12f6e8c1a66405211b8daeff0b2906bd0283f
cyberseekerx <kanekarayush777@proton.me>
2025-07-01 09:28:10 +0530

    Update spotdl-install.sh
    change made here to run thing on other script

--- spotdl-install.sh
+++ spotdl-install.sh
@@ -2,24 +2,21 @@
 
 # Update and install required tools
 pkg update -y && pkg install wget ffmpeg -y
+# Create music folder
+mkdir -p /storage/emulated/0/Music/SpotDL
+
+source ~/.bashrc
+
+# Alias to go to the download folder
+echo 'alias gotospot="cd /storage/emulated/0/Music/SpotDL"' >> ~/.bashrc
+source ~/.bashrc
 
 # Install SpotDL using official script
 curl -sL https://raw.githubusercontent.com/spotDL/spotify-downloader/master/scripts/termux.sh | bash
 
-# Create music folder
-mkdir -p /storage/emulated/0/Music/SpotDL
 
-# Alias to go to the download folder
-echo 'alias gotospot="cd /storage/emulated/0/Music/SpotDL"' >> ~/.bashrc
 
-# Alias for shorter spotdl command
-echo 'alias spot="spotdl"' >> ~/.bashrc
 
-# Reload aliases immediately
-source ~/.bashrc
 
-# Reload shell so aliases work
-source ~/.bashrc
 
-# Go to the folder
-gotospot
+
